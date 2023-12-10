ebc6693159fa028aa55e98999bb359ab8436a80e
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2023-12-10 11:04:48 -0300

    feat: include isRequired validation in MaxLengthFieldValidation

--- src/shared/domain/validations/__tests__/unit/max-length-field.validation.spec.ts
+++ src/shared/domain/validations/__tests__/unit/max-length-field.validation.spec.ts
@@ -18,6 +18,20 @@ describe('MaxLengthFieldValidation unit tests', () => {
     expect(() => sut.validate(dataValidate)).not.toThrow();
   });
 
+  it('should throw a BadRequestError when the given name is not provided if isRequired flag is false', async () => {
+    dataValidate.name = null;
+    expect(() => sut.validate(dataValidate)).toThrow(
+      new BadRequestError(`name is required`),
+    );
+  });
+
+  it('should not throw a BadRequestError when the given name is not provided', async () => {
+    sut = new MaxLengthFieldValidation('name', 100, false);
+    dataValidate.name = null;
+
+    expect(() => sut.validate(dataValidate)).not.toThrow();
+  });
+
   it('should throw a BadRequestError when the given name is not a string', async () => {
     dataValidate.name = 50 as any;
 
