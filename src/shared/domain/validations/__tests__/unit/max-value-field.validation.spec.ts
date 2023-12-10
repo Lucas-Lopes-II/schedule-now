014377c38c8a849dd4c582ce6c0559b82cc76e56
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2023-12-10 11:11:29 -0300

    feat: include isRequired validation in MaxValueFieldValidation

--- src/shared/domain/validations/__tests__/unit/max-value-field.validation.spec.ts
+++ src/shared/domain/validations/__tests__/unit/max-value-field.validation.spec.ts
@@ -18,6 +18,20 @@ describe('MaxValueFieldValidation unit tests', () => {
     expect(() => sut.validate(dataValidate)).not.toThrow();
   });
 
+  it('should throw a BadRequestError when the given age is not provided if isRequired flag is false', async () => {
+    dataValidate.age = null;
+    expect(() => sut.validate(dataValidate)).toThrow(
+      new BadRequestError(`age is required`),
+    );
+  });
+
+  it('should not throw a BadRequestError when the given age is not provided', async () => {
+    sut = new MaxValueFieldValidation('age', 120, false);
+    dataValidate.age = null;
+
+    expect(() => sut.validate(dataValidate)).not.toThrow();
+  });
+
   it('should throw a BadRequestError when the given age is not a number', async () => {
     dataValidate.age = 'test' as any;
 
--- src/shared/domain/validations/max-value-field.validation.ts
+++ src/shared/domain/validations/max-value-field.validation.ts
@@ -1,13 +1,23 @@
-import { Validation } from './validation.inteface';
+import {
+  Validation,
+  RequiredFieldValidation,
+} from '@shared/domain/validations';
 import { BadRequestError } from '@shared/domain/errors';
 
 export class MaxValueFieldValidation<T = any> implements Validation<T> {
   constructor(
     private readonly fieldName: keyof T,
     private readonly maxValue: number,
+    private readonly isRequired: boolean = true,
   ) {}
 
-  validate(input: T): void {
+  public validate(input: T): void {
+    if (this.isRequired) {
+      this.isRequiredValidate(input);
+    } else if (!input[this.fieldName as string]) {
+      return;
+    }
+
     const value = input[this.fieldName] as number;
 
     if (typeof value !== 'number') {
@@ -19,5 +29,10 @@ export class MaxValueFieldValidation<T = any> implements Validation<T> {
         `${this.fieldName as any} must be at most ${this.maxValue}`.trim(),
       );
     }
+  }
+
+  private isRequiredValidate(input: T): void {
+    const isRequiredValidator = new RequiredFieldValidation<T>(this.fieldName);
+    isRequiredValidator.validate(input);
   }
 }
