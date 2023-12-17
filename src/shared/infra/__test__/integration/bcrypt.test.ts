a494178e53271bf79400db8d36fa531115f482dd
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2023-12-17 12:54:20 -0300

    test: Bcrypt: hasher creation

---
+++ src/shared/infra/__test__/integration/bcrypt.test.ts
@@ -0,0 +1,22 @@
+import { bcryptFactory, IHasher } from '@shared/infra/crypto';
+
+describe('Bcrypt integration tests', () => {
+  let sut: IHasher;
+  const dataToHash = 'hash test';
+
+  beforeEach(() => {
+    sut = bcryptFactory();
+  });
+
+  it('should create a hash', async () => {
+    expect.assertions(0);
+    await sut.hash(dataToHash);
+  });
+
+  it('should compare hashes correctly', async () => {
+    const hashedData = await sut.hash(dataToHash);
+
+    await expect(sut.compare(dataToHash, hashedData)).resolves.toBeTruthy();
+    await expect(sut.compare('wrong data', hashedData)).resolves.toBeFalsy();
+  });
+});
