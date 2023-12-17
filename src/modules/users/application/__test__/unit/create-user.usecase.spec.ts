1a7bc5ccc81cea78f5ff42bde742a2ff675234c8
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2023-12-17 13:11:55 -0300

    test: CreateUserUseCase: hashing the password before save on database

--- src/modules/users/application/__test__/unit/create-user.usecase.spec.ts
+++ src/modules/users/application/__test__/unit/create-user.usecase.spec.ts
@@ -1,20 +1,21 @@
-import { randomUUID } from 'node:crypto';
-import { UserDataBuilder } from '@users/tests/mock';
-import { CreateUserUseCase } from '@users/application/usescases';
 import {
   IUserReadingRepo,
   IUserWritingRepo,
 } from '@users/infra/data/repositories';
+import { randomUUID } from 'node:crypto';
+import { IHasher } from '@shared/infra/crypto';
+import { UserDataBuilder } from '@users/tests/mock';
 import { BadRequestError } from '@shared/domain/errors';
+import { CreateUserUseCase } from '@users/application/usescases';
 
 describe('CreateUserUseCase unit tests', () => {
-  const mockedInput = {
+  const mockedInput: CreateUserUseCase.Input = {
     name: 'test',
     email: 'test@example.com',
     password: 'Test@123',
     actionDoneBy: randomUUID(),
   };
-  const mockedOutput = {
+  const mockedOutput: CreateUserUseCase.Output = {
     id: randomUUID(),
     name: mockedInput.name,
     email: mockedInput.email,
@@ -24,6 +25,7 @@ describe('CreateUserUseCase unit tests', () => {
   let sut: CreateUserUseCase.UseCase;
   let mockedUserWritingRepo: IUserWritingRepo;
   let mockedUserReadingRepo: IUserReadingRepo;
+  let mockedHasher: IHasher;
 
   beforeEach(() => {
     mockedUserWritingRepo = {
@@ -32,9 +34,13 @@ describe('CreateUserUseCase unit tests', () => {
     mockedUserReadingRepo = {
       emailExists: jest.fn().mockResolvedValue(false),
     } as any as IUserReadingRepo;
+    mockedHasher = {
+      hash: jest.fn().mockResolvedValue('hashed value'),
+    } as any as IHasher;
     sut = new CreateUserUseCase.UseCase(
       mockedUserWritingRepo,
       mockedUserReadingRepo,
+      mockedHasher,
     );
   });
 
@@ -42,6 +48,9 @@ describe('CreateUserUseCase unit tests', () => {
     const result = await sut.execute(mockedInput);
 
     expect(result).toStrictEqual(mockedOutput);
+    expect(mockedUserWritingRepo.create).toHaveBeenCalledTimes(1);
+    expect(mockedUserReadingRepo.emailExists).toHaveBeenCalledTimes(1);
+    expect(mockedHasher.hash).toHaveBeenCalledTimes(1);
   });
 
   it('should thow a BadRequestError if userReadingRepo.emailExists return true', async () => {
@@ -52,5 +61,31 @@ describe('CreateUserUseCase unit tests', () => {
     expect(sut.execute(mockedInput)).rejects.toThrow(
       new BadRequestError('email already exists'),
     );
+  });
+
+  it('should throw if userReadingRepo.emailExists throws', async () => {
+    jest
+      .spyOn(mockedUserReadingRepo, 'emailExists')
+      .mockImplementationOnce(() => {
+        throw new Error('');
+      });
+
+    expect(sut.execute(mockedInput)).rejects.toThrow();
+  });
+
+  it('should throw if hasher.hash throws', async () => {
+    jest.spyOn(mockedHasher, 'hash').mockImplementationOnce(() => {
+      throw new Error('');
+    });
+
+    expect(sut.execute(mockedInput)).rejects.toThrow();
+  });
+
+  it('should throw if userWritingRepo.create throws', async () => {
+    jest.spyOn(mockedUserWritingRepo, 'create').mockImplementationOnce(() => {
+      throw new Error('');
+    });
+
+    expect(sut.execute(mockedInput)).rejects.toThrow();
   });
 });
